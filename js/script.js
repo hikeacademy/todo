7eeec22569bcf8d3ddc5442f2c6273fcd3aff0b3
chequebsx <gustavotorres@Gustavos-MacBook-Pro-3.local>
2018-04-14 14:28:28 -0300

    Update script

--- js/script.js
+++ js/script.js
@@ -1,113 +1,105 @@
-var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{
-  todo: [],
-  completed: []
-};
-
 const removeIcon = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
-const checkIcon = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
-// const removeIcon = '&#x274C';
-// const checkIcon = '<input id="checkBox" type="checkbox">';
+const completeIcon = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
 
-renderTodoList();
+const todos = (localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : []);
+const completed = (localStorage.getItem('completed') ? JSON.parse(localStorage.getItem('completed')) : []);
+
+loadTasks();
 
 document.getElementById('add').onclick = function() {
-    var newTask = document.getElementById('item').value;
-    if(newTask !== '') {
-        createTask(newTask);
-        document.getElementById('item').value = '';
+    const newTask = document.getElementById('item').value;
+    if(newTask) {
+        addTask(newTask);
     }
-}
+};
 
-document.getElementById('item').addEventListener('keypress', function(e) {
-    var newTask = document.getElementById('item').value;
-    if((event.which == 13 || event.keyCode == 13) && newTask !== '') {
-        createTask(newTask);
-        document.getElementById('item').value = '';
+document.getElementById('item').addEventListener('keypress', function(event) {
+    const newTask = document.getElementById('item').value;
+    if((event.which === 13 || event.keyCode === 13) && newTask !== '') {
+        addTask(newTask);
     }
 });
 
-function createTask(text) {
-    data.todo.push(text);
+function addTask(task) {
+    addItemToDOM(task);
+    document.getElementById('item').value = '';
+    todos.push(task);
     updateStorage();
-    addItemToDOM(text);
 }
 
-function updateStorage() {
-    localStorage.setItem('todoList', JSON.stringify(data));
+function loadTasks() {
+    for(let i = 0; i < todos.length; i += 1) {
+        addItemToDOM(todos[i], false);
+    }
+
+    for(let i = 0; i < completed.length; i += 1) {
+        addItemToDOM(completed[i], true);
+    }
 }
 
 function addItemToDOM(task, completed) {
-    var item = document.createElement('li');
+    const item = document.createElement('li');
     item.innerText = task;
 
-    var buttons = document.createElement('div');
+    const buttons = document.createElement('div');
     buttons.classList.add('buttons');
 
-    var removeButton = document.createElement('button');
+    const removeButton = document.createElement('button');
     removeButton.classList.add('remove');
     removeButton.innerHTML = removeIcon;
-    removeButton.onclick = removeItem;
+    removeButton.addEventListener('click', removeItem);
 
-    var checkButton = document.createElement('button');
-    checkButton.classList.add('complete');
-    checkButton.innerHTML = checkIcon;
-    checkButton.onclick = checkItem;
+    const completeButton = document.createElement('button');
+    completeButton.classList.add('complete');
+    completeButton.innerHTML = completeIcon;
+    completeButton.addEventListener('click', changeItemComplete);
 
-    buttons.appendChild(removeButton);
-    buttons.appendChild(checkButton);
-    item.appendChild(buttons);
+    buttons.append(removeButton);
+    buttons.append(completeButton);
+    item.append(buttons);
 
-    var listName = (completed ? 'completed' : 'todo');
-    let list = document.getElementById(listName);
-    list.prepend(item);
+    const listId = (completed ? 'completed' : 'todo');
+
+    document.getElementById(listId).prepend(item);
 }
 
 function removeItem() {
-    var itemToRemove = this.parentNode.parentNode;
-    var list = itemToRemove.parentNode;
-    var text = itemToRemove.innerText;
+    const item = this.parentNode.parentNode;
+    const text = item.innerText;
+    const currentListId = item.parentNode.id;
 
-    if(list.id === 'todo') {
-        data.todo.splice(data.todo.indexOf(text), 1);
-    } else {
-        data.completed.splice(data.completed.indexOf(text), 1);
-    }
-
-    updateStorage();
-
-    itemToRemove.remove();
-}
-
-function checkItem() {
-    var item = this.parentNode.parentNode;
-    var currentList = item.parentNode;
-    var currentListId = currentList.id;
-    var text = item.innerText;
-
-    if (currentListId === 'todo') {
-        data.todo.splice(data.todo.indexOf(text), 1);
-        data.completed.push(text);
-    } else {
-        data.completed.splice(data.completed.indexOf(text), 1);
-        data.todo.push(text);
-    }
-
-    updateStorage();
     item.remove();
 
-    var newListId = (currentListId === "todo" ? "completed" : "todo");
-    var newList = document.getElementById(newListId);
-    newList.prepend(item);
+    if(currentListId === 'todo') {
+        todos.splice(todos.indexOf(text), 1);
+    } else {
+        completed.splice(completed.indexOf(text), 1);
+    }
+
+    updateStorage();
 }
 
-function renderTodoList() {
-    for(let i = 0; i < data.todo.length; i++) {
-        let item = data.todo[i];
-        addItemToDOM(item);
+function changeItemComplete() {
+    const item = this.parentNode.parentNode;
+    const currentListId = item.parentNode.id;
+    const text = item.innerText;
+
+    item.remove();
+
+    if(currentListId === 'todo') {
+        todos.splice(todos.indexOf(text), 1);
+        completed.push(text);
+        document.getElementById('completed').prepend(item);
+    } else {
+        completed.splice(todos.indexOf(text), 1);
+        todos.push(text);
+        document.getElementById('todo').prepend(item);
     }
 
-    for(let i = 0; i < data.completed.length; i++) {
-        let item = data.completed[i];
-        addItemToDOM(item, true);
-    }
+    updateStorage();
+}
+
+function updateStorage() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem('completed', JSON.stringify(completed));
 }
